a50c159e3aa647b9b8ceeaf272117d6eccc8b460
Sanghwa Lee <jaja_spider@naver.com>
2020-07-23 15:00:15 +0900

    15649

--- 15649.py
+++ 15649.py
@@ -6,8 +6,7 @@ check_list = [False] * N
 arr = []
 
 
-def dfs(cnt,start):
-    # 주어진 개수만큼 채워지면 출력한다
+def dfs(cnt, start):
     if cnt == M:
         print(*arr)
         return
@@ -16,17 +15,11 @@ def dfs(cnt,start):
         if check_list[i]:
             continue
 
-        # i번째는 거쳐갈거라서 True로 바꾼다.
         check_list[i] = True
         arr.append(num_list[i])
-        # 현재의 i를 기준으로 가지치기 시작
-        dfs(cnt + 1,i+1)
-        # 이 부분은
+        dfs(cnt + 1, i + 1)
         arr.pop()
-        # 여기서 print(arr)을 해보면 작동원리를 알 수 있다.
-        #         print(arr)
-        #         print(check_list)
         check_list[i] = False
 
 
-dfs(0,0)
+dfs(0, 0)
