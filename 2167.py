d4c150961d780624a2a28970a02320c42990315d
Sanghwa Lee <jaja_spider@naver.com>
2020-08-04 11:47:11 +0900

    2167, 3943

--- 2167.py
+++ 2167.py
@@ -1,11 +1,14 @@
-N, M = map(int, input().split())
-matrix = [[] for _ in range(N)]
-for i in range(0, int(N)):
-    temp = list(map(int, input().split()))
-    matrix[i].extend(temp)
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+# matrix = [[] * M for _ in range(0, N)]
+matrix = []
+
+for _ in range(0, N):
+    matrix.append(list(map(int, sys.stdin.readline().split())))
 countlist = []
-for _ in range(int(input())):
-    i, j, x, y = map(int, input().split())
+for _ in range(int(sys.stdin.readline())):
+    i, j, x, y = map(int, sys.stdin.readline().split())
     count = 0
     for A in range(i - 1, x):
         for B in range(j - 1, y):
---
+++ 3943.py
@@ -0,0 +1,15 @@
+import sys
+
+T = int(sys.stdin.readline())
+for _ in range(0, T):
+    hailstone = []
+    n = int(sys.stdin.readline())
+    hailstone.append(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+            hailstone.append(int(n))
+        elif n % 2 == 1:
+            n = n * 3 + 1
+            hailstone.append(int(n))
+    print(max(hailstone))
