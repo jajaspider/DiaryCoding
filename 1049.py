c3773d88c7015465106a073a62102bf1d9260bbc
jajaspider <sonaapi17@gmail.com>
2020-09-21 22:22:28 +0900

    1049 Solved

---
+++ 1049.py
@@ -0,0 +1,17 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+six = 1001
+one = 1001
+for _ in range(0, M):
+    A, B = map(int, sys.stdin.readline().split())
+    six = six if six < A else A
+    one = one if one < B else B
+
+if N > 6:
+    x = N // 6
+    y = N % 6
+    price = [(x + 1) * six, x * six + y * one]
+    print(min(price))
+else:
+    print(six if six < N * one else N * one)
