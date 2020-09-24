f602ff8ba7ecd05181fca0cbb77035d67cefac9b
jajaspider <jaja_spider@naver.com>
2020-09-24 19:14:52 +0900

    2310 fix

--- 2310.py
+++ 2310.py
@@ -3,19 +3,25 @@ import sys
 sys.setrecursionlimit(100000)
 
 
-def dfs(current, visit, money):
+def dfs(current, N, visit, money):
     visit.append(current)
     if matrix[current - 1][0] == 'L':
         if money <= int(matrix[current - 1][1]):
             money = int(matrix[current - 1][1])
-    if matrix[current - 1][0] == 'T':
+    elif matrix[current - 1][0] == 'T':
         money -= int(matrix[current - 1][1])
+        if money < 0:
+            return
+
+    if current == N:
         moneys.append(money)
+        return
+
     for i in range(2, len(matrix[current - 1])):
         if matrix[current - 1][i] == 0:
-            break
+            return
         if int(matrix[current - 1][i]) not in visit:
-            dfs(int(matrix[current - 1][i]), visit, money)
+            dfs(int(matrix[current - 1][i]), N, visit, money)
 
 
 while True:
@@ -28,9 +34,9 @@ while True:
     moneys = []
     for i in range(0, N):
         matrix.append(sys.stdin.readline().replace("\n", "").split(" "))
-    print(matrix)
+    # print(matrix)
     flag = 0
-    dfs(1, visit, 0)
+    dfs(1, N, visit, 0)
     for i in range(0, len(moneys)):
         if int(moneys[i]) >= 0:
             print("Yes")
