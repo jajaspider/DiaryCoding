bfd7e770bd1eb449061d61573023bbd687566cf7
jajaspider <jaja_spider@naver.com>
2020-11-03 14:37:36 +0900

    1316 java

--- boj1316.java
+++ boj1316.java
@@ -6,28 +6,26 @@ public class boj1316 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int count=0;
-        for(int i=0;i<N;i+=1){
+        for(int i=0;i<N+1;i+=1){
             boolean groud_word = false;
             String previous_word = "";
             String Nword_string = sc.nextLine();
             char[] Nword = Nword_string.toCharArray();
             for(int j=0;j<Nword.length;j+=1){
-                System.out.println(previous_word.indexOf(Nword[j]));
-                System.out.println("-------------");
                 if(previous_word.lastIndexOf(Nword[j])==-1){
                     previous_word+=Nword[j];
                     groud_word=true;
                     continue;
                 }
-                if(previous_word.lastIndexOf(Nword[j])==1){
+                else if(previous_word.lastIndexOf(Nword[j])==previous_word.length()-1){
                     groud_word=true;
                 }
-                else if(previous_word.lastIndexOf(Nword[j])>=2){
+                else {
                     groud_word=false;
                     break;
                 }
             }
-            System.out.println(previous_word);
+//            System.out.println(previous_word);
             if(groud_word){
                 count+=1;
             }
